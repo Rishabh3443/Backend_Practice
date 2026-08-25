1e0688fa7a2339281da6bef9352d9c9a1dae53c8
Rishabh3443 <risabhtri2478@gmail.com>
2026-08-25 20:04:31 +0530

    make all feature for crud

--- backend_day3/src/controllers/notes.controller.js
+++ backend_day3/src/controllers/notes.controller.js
@@ -1,24 +1,88 @@
-const NotesModel = require("../model/notesModel")
+const NotesModel = require("../model/notesModel");
 
-const createNotesController = async (req,res)=>{
-    try {
-        
-        let {title,description,} = req.body;
+const createNotesController = async (req, res) => {
+  try {
+    let { title, description } = req.body;
 
     let newNotes = await NotesModel.create({
-        title,
-        description,
-    })
+      title,
+      description,
+    });
 
     return res.status(201).json({
-        message:" your notes created successfully",
-        data: newNotes,
-    })
-    } catch (error) {
-      return  res.status(500).json({
-            message:error.message,
-        })
-    }
+      message: " your notes created successfully",
+      data: newNotes,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-module.exports = createNotesController
+const getAllNotesController = async (req, res) => {
+  try {
+    const allNotes = await NotesModel.find();
+    res.status(201).json({
+      message: "all notes are here👇🏻👇🏻",
+      data: allNotes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "error.message",
+    });
+  };
+};
+  const getSingleNotesController = async (req, res) => {
+    try {
+      let noteid = req.params.id;
+      let singleNotes = await NotesModel.findById(noteid);
+      res.status(201).json({
+        message: "here is your single note",
+        data: singleNotes,
+      });
+    } catch (error) {
+      res.status(500).json({
+        error: error.message,
+      });
+    }
+  };
+
+  const updatenotesController = async(req,res)=>{
+    try {
+         let noteId = req.params.id;
+         let body = req.body;
+     let updatedNotes = await NotesModel.findByIdAndUpdate(noteId,body,{new:true});
+     res.status(201).json({
+        message:"here is your updated notes",
+        data:updatedNotes
+     })
+    } catch (error) {
+         res.status(500).json({
+        error: error.message,
+      });
+    }
+  };
+
+  const deleteNotesController = async (req,res)=>{
+   try {
+    let notesid = req.params.id;
+   let deletedNotes = await NotesModel.findByIdAndDelete(notesid);
+   res.status(201).json({
+    message: "your notes deleted successfully"
+   })
+
+   } catch (error) {
+    res.status(500).json({
+        error: error.message,
+      });
+   }
+  }
+
+module.exports = {
+  createNotesController,
+  getAllNotesController,
+  getSingleNotesController,
+  updatenotesController,
+  deleteNotesController
+};
--- backend_day3/src/routes/notes.route.js
+++ backend_day3/src/routes/notes.route.js
@@ -1,10 +1,24 @@
 const express = require("express");
 
 const router = express.Router();
-const createNotesController = require("../controllers/notes.controller")
+const {
+  createNotesController,
+  getAllNotesController,
+ getSingleNotesController,
+ updatenotesController,
+ deleteNotesController
+} = require("../controllers/notes.controller");
 
 
+router.post("/create", createNotesController);
 
-router.post("/create",createNotesController);
 
+router.get("/allnotes", getAllNotesController);
+
+
+router.get("/:id",getSingleNotesController);
+
+router.put("/:id",updatenotesController);
+
+router.delete("/:id",deleteNotesController);
 module.exports = router;
