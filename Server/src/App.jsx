1981872e475e2242e038d2799ce15c84e8221a3a
Rishabh3443 <risabhtri2478@gmail.com>
2026-08-30 18:25:37 +0530

    Build: connecting backend to frontend

---
+++ Server/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='Enter your name'/>
+         <input type="text" placeholder='Enter your email' />
+          <input type="text" />
+          <button>Create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App;
